c1b43df3ff2bffe551cb7e1fbc0fa410ce54b27c
harshil <harshilr1909@gmail.com>
2026-08-05 06:36:45 +0530

    neo-tree added

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    { "<leader>e", ":Neotree toggle<CR>", desc = "Toggle file tree" },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = { visible = true },
+      follow_current_file = true,
+    },
+  },
+}
